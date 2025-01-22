b86fc0ae3fd781f0c741da337f2118a591b828f9
AJAY C <95734728+AjayGaneshC@users.noreply.github.com>
2025-01-22 15:23:47 +0530

    Update my_config.py

--- configs/my_config.py
+++ configs/my_config.py
@@ -9,11 +9,13 @@ class MyConfig(BaseConfig):
         self.subset = 'kvasir'
         self.data_root = '/path/to/your/dataset'
         self.use_test_set = True
-
+        self.num_channel = 3  # Number of input image channels
+        self.num_class = 1    # Number of output classes
+        self.base_channel = 32  # Base number of channels
         # Model
         self.model = 'unet'
         self.base_channel = 32
-
+        self.model_path = r'C:\Users\HP\Downloads\best.pth'  # Full path to your trained model
         # Training
         self.total_epoch = 400
         self.train_bs = 16
